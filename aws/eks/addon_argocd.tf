f1f6b28b1ef0053b2a67e09ab3b1fd9eef904b59
Leroy Kayanda <leroy@carry1st.com>
2024-03-17 19:02:56 +0300

    setting up eks module

--- aws/eks/addon_argocd.tf
+++ aws/eks/addon_argocd.tf
@@ -1,43 +1,43 @@
-# resource "kubernetes_namespace" "ns" {
-#   count = var.cluster_created ? 1 : 0
-#   metadata {
-#     name = "argocd"
-#   }
-# }
+resource "kubernetes_namespace" "ns" {
+  count = var.cluster_created ? 1 : 0
+  metadata {
+    name = "argocd"
+  }
+}
 
-# resource "helm_release" "argocd" {
-#   count      = var.cluster_created ? 1 : 0
-#   name       = "argo-cd"
-#   repository = "https://argoproj.github.io/argo-helm"
-#   chart      = "argo-cd"
-#   namespace  = "argocd"
-#   version    = "5.52.1"
+resource "helm_release" "argocd" {
+  count      = var.cluster_created ? 1 : 0
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  version    = "6.7.2"
 
-#   set {
-#     name  = "server.service.type"
-#     value = "NodePort"
-#   }
+  set {
+    name  = "server.service.type"
+    value = "NodePort"
+  }
 
-#   set {
-#     name  = "notifications.secret.create"
-#     value = false
-#   }
+  set {
+    name  = "notifications.secret.create"
+    value = false
+  }
 
-#   set {
-#     name  = "notifications.cm.create"
-#     value = false
-#   }
+  set {
+    name  = "notifications.cm.create"
+    value = false
+  }
 
-#   set {
-#     name  = "notifications.containerPorts.metrics"
-#     value = 9002
-#   }
+  set {
+    name  = "notifications.containerPorts.metrics"
+    value = 9002
+  }
 
-#   values = [
-#     <<EOT
-# configs:
-#   cm:
-#     "timeout.reconciliation": "60s"
-# EOT
-#   ]
-# }
+  values = [
+    <<EOT
+configs:
+  cm:
+    "timeout.reconciliation": "60s"
+EOT
+  ]
+}
