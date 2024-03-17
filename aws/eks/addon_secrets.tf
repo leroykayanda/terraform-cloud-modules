b2c8b675c079a8e2cb7bbfd5f698861d9ff2412b
Leroy Kayanda <leroy@carry1st.com>
2024-03-17 18:50:53 +0300

    setting up eks module

--- aws/eks/addon_secrets.tf
+++ aws/eks/addon_secrets.tf
@@ -1,1 +1,7 @@
-
+resource "helm_release" "secrets" {
+  count      = var.cluster_created ? 1 : 0
+  name       = "external-secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  namespace  = "kube-system"
+}
