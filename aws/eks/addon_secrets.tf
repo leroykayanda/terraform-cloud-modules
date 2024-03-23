9d8ad72319806f9958b9e9ed7336e4389debe8e3
Leroy Kayanda <leroykayanda@gmail.com>
2024-03-23 18:18:59 +0300

    setting up stakater reloader

--- aws/eks/addon_secrets.tf
+++ aws/eks/addon_secrets.tf
@@ -6,3 +6,16 @@ resource "helm_release" "secrets" {
   chart      = "external-secrets"
   namespace  = "kube-system"
 }
+
+resource "helm_release" "reloader" {
+  count      = var.cluster_created ? 1 : 0
+  name       = "reloader"
+  repository = "https://stakater.github.io/stakater-charts"
+  chart      = "reloader"
+  namespace  = "kube-system"
+
+  set {
+    name  = "reloader.reloadStrategy"
+    value = "annotations"
+  }
+}
