03aa32b202e62cd2ff73cedf20e5bb60434a1b15
Leroy Kayanda <leroykayanda@gmail.com>
2024-05-27 15:24:46 +0300

    setting up karpenter

--- aws/eks/locals.tf
+++ aws/eks/locals.tf
@@ -1,4 +1,4 @@
 locals {
   eks_oidc_issuer = var.cluster_created ? trimprefix(data.aws_eks_cluster.cluster[0].identity[0].oidc[0].issuer, "https://") : ""
-  karpenter_sa    = "karpenter_sa"
+  karpenter_sa    = "karpenter-sa"
 }
