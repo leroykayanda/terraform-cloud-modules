1d21dc4f1f54a6d802ab390e6018c8c0ab283a3b
Leroy Kayanda <leroykayanda@gmail.com>
2024-05-27 12:17:02 +0300

    setting up karpenter

--- aws/eks/app_argocd.tf
+++ aws/eks/app_argocd.tf
@@ -207,6 +207,6 @@ resource "helm_release" "image_updater" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argocd-image-updater"
   namespace  = "argocd"
-  version    = "0.9.2"
+  version    = "0.10.0"
   values     = var.argocd_image_updater_values
 }
