11b6a2425172f4872599f4469eeabcd8d967c1e2
Leroy Kayanda <leroykayanda@gmail.com>
2024-05-27 12:54:12 +0300

    setting up karpenter

--- aws/eks/app_karpenter.tf
+++ aws/eks/app_karpenter.tf
@@ -1,27 +1,8 @@
-resource "helm_release" "insights" {
+resource "helm_release" "karpenter" {
   count      = var.cluster_created && var.autoscaling_type == "karpenter" ? 1 : 0
   name       = "karpenter"
   repository = "https://charts.karpenter.sh/"
   chart      = "karpenter"
   namespace  = "kube-system"
   version    = "0.16.3"
-
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = var.container_insights_service_name
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = false
-  }
-
-  depends_on = [
-    kubernetes_service_account.container_insights_sa
-  ]
 }
