99659118adba499408354adc385b4d33670dc86b
Leroy Kayanda <leroykayanda@gmail.com>
2024-05-29 00:28:52 +0300

    setting up karpenter

--- aws/eks/app_karpenter.tf
+++ aws/eks/app_karpenter.tf
@@ -290,95 +290,94 @@ resource "aws_iam_role_policy_attachment" "instance_profile" {
 }
 
 
-# resource "kubernetes_manifest" "nodepools" {
-#   count = var.cluster_created && var.autoscaling_type == "karpenter" ? 1 : 0
+resource "kubernetes_manifest" "nodepools" {
+  count = var.cluster_created && var.autoscaling_type == "karpenter" ? 1 : 0
 
-#   manifest = {
-#     apiVersion = "karpenter.sh/v1beta1"
-#     kind       = "NodePool"
-#     metadata = {
-#       name = "default"
-#     }
-#     spec = {
-#       template = {
-#         spec = {
-#           nodeClassRef = {
-#             apiVersion = "karpenter.k8s.aws/v1beta1"
-#             kind       = "EC2NodeClass"
-#             name       = "default"
-#           }
-#           requirements = [
-#             {
-#               key      = "karpenter.sh/capacity-type"
-#               operator = "In"
-#               values   = ["on-demand"]
-#             },
-#             {
-#               key      = "node.kubernetes.io/instance-type"
-#               operator = "In"
-#               values   = "${var.karpenter["instance_types"]}"
-#             }
-#           ]
-#         }
-#       }
-#       limits = {
-#         cpu    = "${var.karpenter["cpu_limit"]}"
-#         memory = "${var.karpenter["memory_limit"]}"
-#       }
-#       disruption = {
-#         consolidationPolicy = "WhenUnderutilized"
-#         budgets = [
-#           {
-#             nodes = "${var.karpenter["disruption_budget"]}"
-#           }
-#         ]
-#       }
-#     }
-#   }
-# }
+  manifest = {
+    apiVersion = "karpenter.sh/v1beta1"
+    kind       = "NodePool"
+    metadata = {
+      name = "default"
+    }
+    spec = {
+      template = {
+        spec = {
+          nodeClassRef = {
+            apiVersion = "karpenter.k8s.aws/v1beta1"
+            kind       = "EC2NodeClass"
+            name       = "default"
+          }
+          requirements = [
+            {
+              key      = "karpenter.sh/capacity-type"
+              operator = "In"
+              values   = ["on-demand"]
+            },
+            {
+              key      = "node.kubernetes.io/instance-type"
+              operator = "In"
+              values   = "${var.karpenter["instance_types"]}"
+            }
+          ]
+        }
+      }
+      limits = {
+        cpu    = "${var.karpenter["cpu_limit"]}"
+        memory = "${var.karpenter["memory_limit"]}"
+      }
+      disruption = {
+        consolidationPolicy = "WhenUnderutilized"
+        budgets = [
+          {
+            nodes = "${var.karpenter["disruption_budget"]}"
+          }
+        ]
+      }
+    }
+  }
+}
 
-# resource "kubernetes_manifest" "karpenter_node_template" {
-#   count = var.cluster_created && var.autoscaling_type == "karpenter" ? 1 : 0
+resource "kubernetes_manifest" "karpenter_node_template" {
+  count = var.cluster_created && var.autoscaling_type == "karpenter" ? 1 : 0
 
-#   manifest = {
-#     apiVersion = "karpenter.k8s.aws/v1beta1"
-#     kind       = "EC2NodeClass"
-#     metadata = {
-#       name = "default"
-#     }
-#     spec = {
-#       amiFamily = "Bottlerocket"
-#       securityGroupSelectorTerms = [
-#         {
-#           tags = {
-#             "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-#           }
-#         }
-#       ]
-#       subnetSelectorTerms = [
-#         {
-#           tags = {
-#             "${var.karpenter["karpenter_subnet_key"]}" = "${var.karpenter["karpenter_subnet_value"]}"
-#           }
-#         }
-#       ]
-#       #instanceProfile = aws_iam_instance_profile.karpenter[0].name
-#       role = aws_iam_role.karpenter_instance_profile_role[0].name
-#       tags = {
-#         Team        = var.team
-#         Environment = var.env
-#       }
-#       blockDeviceMappings = [
-#         {
-#           deviceName = "/dev/xvda"
-#           ebs = {
-#             volumeSize          = var.karpenter["disk_size"]
-#             volumeType          = "gp3"
-#             deleteOnTermination = true
-#           }
-#         }
-#       ]
-#     }
-#   }
-# }
+  manifest = {
+    apiVersion = "karpenter.k8s.aws/v1beta1"
+    kind       = "EC2NodeClass"
+    metadata = {
+      name = "default"
+    }
+    spec = {
+      amiFamily = "Bottlerocket"
+      securityGroupSelectorTerms = [
+        {
+          tags = {
+            "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+          }
+        }
+      ]
+      subnetSelectorTerms = [
+        {
+          tags = {
+            "${var.karpenter["karpenter_subnet_key"]}" = "${var.karpenter["karpenter_subnet_value"]}"
+          }
+        }
+      ]
+      instanceProfile = aws_iam_instance_profile.karpenter[0].name
+      tags = {
+        Team        = var.team
+        Environment = var.env
+      }
+      blockDeviceMappings = [
+        {
+          deviceName = "/dev/xvda"
+          ebs = {
+            volumeSize          = var.karpenter["disk_size"]
+            volumeType          = "gp3"
+            deleteOnTermination = true
+          }
+        }
+      ]
+    }
+  }
+}
 
