87145457b0c3963969e84d59799125abfe54df24
Leroy Kayanda <leroykayanda@gmail.com>
2024-05-30 23:13:18 +0300

    setting up karpenter

--- aws/eks/app_elk.tf
+++ aws/eks/app_elk.tf
@@ -129,7 +129,7 @@ resource "helm_release" "kibana" {
 # kibana ingress
 
 resource "kubernetes_ingress_v1" "kibana" {
-  count = var.cluster_created ? 1 : 0
+  count = var.cluster_created && var.logs_type == "elk" ? 1 : 0
   metadata {
     name      = "kibana"
     namespace = "elk"
@@ -144,6 +144,7 @@ resource "kubernetes_ingress_v1" "kibana" {
       "alb.ingress.kubernetes.io/load-balancer-attributes" = var.argocd["load_balancer_attributes"]
       "alb.ingress.kubernetes.io/target-group-attributes"  = var.argocd["target_group_attributes"]
       "alb.ingress.kubernetes.io/tags"                     = var.argocd["tags"]
+      "alb.ingress.kubernetes.io/target-type"              = "ip"
       "alb.ingress.kubernetes.io/group.name"               = var.env
     }
   }
