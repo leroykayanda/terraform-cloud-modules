d67b6557d90b0294973b3a1c2cbe0502395ff89a
Leroy Kayanda <leroykayanda@gmail.com>
2024-06-04 09:54:22 +0300

    setting up karpenter

--- aws/eks/app_karpenter.tf
+++ aws/eks/app_karpenter.tf
@@ -329,7 +329,7 @@ spec:
         values: ["on-demand"]
       - key: node.kubernetes.io/instance-type
         operator: In
-        values: ${indent(8, yamlencode(var.karpenter["instance_types"]))}
+        values: [join(",",${var.karpenter["instance_types"]})]
   limits:
     cpu: "${var.karpenter["cpu_limit"]}"
     memory: "${var.karpenter["memory_limit"]}"
