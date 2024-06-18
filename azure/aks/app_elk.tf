c3b41ed4f5e43d4c5359db056e87cacadb7abbba
Leroy Kayanda <leroykayanda@gmail.com>
2024-06-18 17:23:32 +0300

    aks - setting up monitoring

--- azure/aks/app_elk.tf
+++ azure/aks/app_elk.tf
@@ -1,84 +1,84 @@
-# # elk namespace
+# elk namespace
 
-# resource "kubernetes_namespace" "elk" {
-#   count = var.cluster_created ? 1 : 0
-#   metadata {
-#     name = "elk"
-#   }
-# }
+resource "kubernetes_namespace" "elk" {
+  count = var.cluster_created ? 1 : 0
+  metadata {
+    name = "elk"
+  }
+}
 
-# # elasticsearch
+# elasticsearch
 
-# resource "helm_release" "elastic" {
-#   count      = var.cluster_created ? 1 : 0
-#   name       = "elasticsearch"
-#   chart      = "elasticsearch"
-#   version    = "8.5.1"
-#   repository = "https://helm.elastic.co"
-#   namespace  = "elk"
+resource "helm_release" "elastic" {
+  count      = var.cluster_created ? 1 : 0
+  name       = "elasticsearch"
+  chart      = "elasticsearch"
+  version    = "8.5.1"
+  repository = "https://helm.elastic.co"
+  namespace  = "elk"
 
-#   set {
-#     name  = "replicas"
-#     value = var.elastic["replicas"]
-#   }
+  set {
+    name  = "replicas"
+    value = var.elastic["replicas"]
+  }
 
-#   set {
-#     name  = "minimumMasterNodes"
-#     value = var.elastic["minimumMasterNodes"]
-#   }
+  set {
+    name  = "minimumMasterNodes"
+    value = var.elastic["minimumMasterNodes"]
+  }
 
-#   #   set {
-#   #     name  = "volumeClaimTemplate.storageClassName"
-#   #     value = "elasticsearch"
-#   #   }
+  #   set {
+  #     name  = "volumeClaimTemplate.storageClassName"
+  #     value = "elasticsearch"
+  #   }
 
-#   set {
-#     name  = "volumeClaimTemplate.resources.requests.storage"
-#     value = var.elastic["pv_storage"]
-#   }
+  set {
+    name  = "volumeClaimTemplate.resources.requests.storage"
+    value = var.elastic["pv_storage"]
+  }
 
-#   set {
-#     name  = "createCert"
-#     value = "true"
-#   }
+  set {
+    name  = "createCert"
+    value = "true"
+  }
 
-#   set {
-#     name  = "protocol"
-#     value = "https"
-#   }
+  set {
+    name  = "protocol"
+    value = "https"
+  }
 
-#   set {
-#     name  = "secret.password"
-#     value = var.elastic_password
-#   }
+  set {
+    name  = "secret.password"
+    value = var.elastic_password
+  }
 
-#   values = [
-#     <<EOF
-#     esConfig: 
-#       elasticsearch.yml: |
-#         xpack.security.enabled: true
-#     resources: 
-#       requests:
-#         cpu: "500m"
-#         memory: "1Gi"
-#       limits:
-#         cpu: "1000m"
-#         memory: "2Gi"
-#     tolerations:
-#     - key: "priority"
-#       operator: "Equal"
-#       value: "critical"
-#       effect: "NoSchedule"
-#     nodeSelector:
-#       priority: "critical"
-#     EOF
-#   ]
+  values = [
+    <<EOF
+    esConfig: 
+      elasticsearch.yml: |
+        xpack.security.enabled: true
+    resources: 
+      requests:
+        cpu: "500m"
+        memory: "1Gi"
+      limits:
+        cpu: "1000m"
+        memory: "2Gi"
+    tolerations:
+    - key: "priority"
+      operator: "Equal"
+      value: "critical"
+      effect: "NoSchedule"
+    nodeSelector:
+      priority: "critical"
+    EOF
+  ]
 
-#   depends_on = [
-#     kubernetes_namespace.elk
-#   ]
+  depends_on = [
+    kubernetes_namespace.elk
+  ]
 
-# }
+}
 
 # # kibana helm chart
 
