fdbf9f7189d8ad6baf8aba1d9bcc94874e6b5311
Leroy Kayanda <leroykayanda@gmail.com>
2024-06-24 10:58:57 +0300

    aks - setting up monitoring

--- azure/aks/app_grafana.tf
+++ azure/aks/app_grafana.tf
@@ -123,95 +123,102 @@ resource "kubernetes_ingress_v1" "prometheus" {
 
 }
 
-# # grafana helm chart
+# grafana helm chart
 
-# resource "helm_release" "grafana" {
-#   count      = var.cluster_created ? 1 : 0
-#   name       = "grafana"
-#   chart      = "grafana"
-#   version    = "7.3.11"
-#   repository = "https://grafana.github.io/helm-charts"
-#   namespace  = "grafana"
+resource "helm_release" "grafana" {
+  count      = var.cluster_created ? 1 : 0
+  name       = "grafana"
+  chart      = "grafana"
+  version    = "7.3.11"
+  repository = "https://grafana.github.io/helm-charts"
+  namespace  = "grafana"
 
-#   set {
-#     name  = "persistence.enabled"
-#     value = "true"
-#   }
+  set {
+    name  = "persistence.enabled"
+    value = "true"
+  }
 
-#   set {
-#     name  = "persistence.size"
-#     value = var.grafana["pv_storage"]
-#   }
+  set {
+    name  = "persistence.size"
+    value = var.grafana["pv_storage"]
+  }
 
-#   set {
-#     name  = "persistence.storageClassName"
-#     value = var.grafana["storageClassName"]
-#   }
+  set {
+    name  = "persistence.storageClassName"
+    value = var.grafana["storageClassName"]
+  }
 
-#   set {
-#     name  = "adminPassword"
-#     value = var.grafana_password
-#   }
+  set {
+    name  = "adminPassword"
+    value = var.grafana_password
+  }
 
-#   values = [
-#     <<EOF
-# datasources:
-#   datasources.yaml:
-#     apiVersion: 1
-#     datasources:
-#       - name: Prometheus
-#         type: prometheus
-#         url: http://prometheus-server.grafana
-#         access: proxy
-#         isDefault: true
-# dashboardProviders:
-#     dashboardproviders.yaml:
-#         apiVersion: 1
-#         providers:
-#         - name: 'default'
-#           orgId: 1
-#           folder: ''
-#           type: file
-#           disableDeletion: false
-#           editable: true
-#           options:
-#             path: /var/lib/grafana/dashboards/default
-# dashboards:
-#   default:
-#     kubernetes-dashboard:
-#       json: |
-#         ${indent(8, file("${path.module}/dashboard.json"))}
-# grafana.ini:
-#   server:
-#     domain: "${var.grafana["dns_name"]}.${var.dns_zone}"
-#     root_url: "%(protocol)s://%(domain)s/"
-# alerting: 
-#   contactpoints.yaml:
-#     secret:
-#       apiVersion: 1
-#       contactPoints:
-#         - orgId: 1
-#           name: slack_alerts
-#           receivers:
-#             - uid: slack
-#               type: slack
-#               settings:
-#                 url: "${var.slack_incoming_webhook_url}"
-#   rules.yaml:
-#     apiVersion: 1
-#     groups:
-#       - orgId: 1
-#         name: evaluation_group
-#         folder: "${var.env}-env"
-#         interval: 5m
-#   policies.yaml:
-#     policies:
-#       - orgId: 1
-#         receiver: slack_alerts
-# EOF
-#   ]
+  values = [
+    <<EOF
+tolerations:
+- key: "priority"
+  operator: "Equal"
+  value: "critical"
+  effect: "NoSchedule"
+nodeSelector:
+  priority: "critical"
+datasources:
+  datasources.yaml:
+    apiVersion: 1
+    datasources:
+      - name: Prometheus
+        type: prometheus
+        url: http://prometheus-server.grafana
+        access: proxy
+        isDefault: true
+dashboardProviders:
+    dashboardproviders.yaml:
+        apiVersion: 1
+        providers:
+        - name: 'default'
+          orgId: 1
+          folder: ''
+          type: file
+          disableDeletion: false
+          editable: true
+          options:
+            path: /var/lib/grafana/dashboards/default
+dashboards:
+  default:
+    kubernetes-dashboard:
+      json: |
+        ${indent(8, file("${path.module}/dashboard.json"))}
+grafana.ini:
+  server:
+    domain: "${var.grafana["dns_name"]}.${var.dns_zone}"
+    root_url: "%(protocol)s://%(domain)s/"
+alerting: 
+  contactpoints.yaml:
+    secret:
+      apiVersion: 1
+      contactPoints:
+        - orgId: 1
+          name: slack_alerts
+          receivers:
+            - uid: slack
+              type: slack
+              settings:
+                url: "${var.slack_incoming_webhook_url}"
+  rules.yaml:
+    apiVersion: 1
+    groups:
+      - orgId: 1
+        name: evaluation_group
+        folder: "${var.env}-env"
+        interval: 5m
+  policies.yaml:
+    policies:
+      - orgId: 1
+        receiver: slack_alerts
+EOF
+  ]
 
-# }
+}
 
 # # grafana dns name
 
