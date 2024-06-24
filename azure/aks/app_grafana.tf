2b30a939c8f126ba9f3a9db73da231364dc47b16
Leroy Kayanda <leroykayanda@gmail.com>
2024-06-24 10:51:04 +0300

    aks - setting up monitoring

--- azure/aks/app_grafana.tf
+++ azure/aks/app_grafana.tf
@@ -57,6 +57,72 @@ resource "azurerm_dns_a_record" "prometheus" {
   target_resource_id  = azurerm_public_ip.ip.id
 }
 
+# prometheus tls secret
+
+resource "kubernetes_secret" "prometheus" {
+  count = var.cluster_created ? 1 : 0
+  metadata {
+    name      = "prometheus-tls-cert"
+    namespace = "elk"
+  }
+
+  type = "kubernetes.io/tls"
+
+  data = {
+    "tls.key" = ""
+    "tls.crt" = ""
+  }
+
+  lifecycle {
+    ignore_changes = [metadata]
+  }
+}
+
+# prometheus ingress
+
+resource "kubernetes_ingress_v1" "prometheus" {
+  count = var.cluster_created ? 1 : 0
+  metadata {
+    name      = "prometheus"
+    namespace = "grafana"
+    annotations = {
+      "kubernetes.io/ingress.class"                                  = "azure/application-gateway"
+      "appgw.ingress.kubernetes.io/health-probe-unhealthy-threshold" = "2"
+      "appgw.ingress.kubernetes.io/request-timeout"                  = "60"
+      "appgw.ingress.kubernetes.io/ssl-redirect"                     = "true"
+      "cert-manager.io/cluster-issuer"                               = "letsencrypt-issuer"
+      "appgw.ingress.kubernetes.io/backend-protocol"                 = "HTTP"
+    }
+  }
+
+  spec {
+    tls {
+      hosts       = ["${var.prometheus["dns_name"]}.${var.dns_zone}"]
+      secret_name = "prometheus-tls-cert"
+    }
+
+    rule {
+      host = "${var.prometheus["dns_name"]}.${var.dns_zone}"
+
+      http {
+        path {
+          path = "/"
+
+          backend {
+            service {
+              name = "prometheus-server"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
+
 # # grafana helm chart
 
 # resource "helm_release" "grafana" {
