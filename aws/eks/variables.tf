0fa99b7c4c8e0aabe193cfc7023444fc31962bcf
Leroy Kayanda <leroykayanda@gmail.com>
2024-09-26 21:06:35 +0300

    eks module changes

--- aws/eks/variables.tf
+++ aws/eks/variables.tf
@@ -7,61 +7,20 @@ variable "region" {
   type = string
 }
 
-variable "sns_topic" {
-  type        = string
-  description = "SNS topic ARN for notifications"
-  default     = null
-}
-
-variable "company_name" {
-  type        = string
-  description = "To make bucket name unique"
-  default     = "contoso"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Used to tag resources"
-  default     = {}
-}
-
-variable "cluster_tags" {
-  type        = map(string)
-  description = "Used to tag the EKS cluster"
-  default     = {}
-}
-
-# EKS
 variable "cluster_created" {
+  type        = bool
   description = "create applications such as argocd only when the eks cluster has already been created"
   default     = false
 }
 
-variable "metrics_type" {
-  description = "cloudwatch or prometheus-grafana"
-  default     = "prometheus-grafana"
-}
-
-variable "logs_type" {
-  description = "cloudwatch or elk"
-  default     = "elk"
-}
-
-variable "autoscaling_type" {
-  description = "cluster-autoscaler or karpenter"
-  default     = "karpenter"
-}
-
 variable "cluster_name" {
-  type = string
+  type        = string
+  description = "EKS cluster name"
 }
 
 variable "cluster_version" {
-  type = string
-}
-
-variable "cluster_endpoint_public_access" {
-  default = true
+  type        = string
+  description = "EKS cluster version"
 }
 
 variable "vpc_id" {
@@ -76,14 +35,81 @@ variable "private_subnets" {
   type = list(string)
 }
 
-variable "capacity_type" {
-  description = "Type of capacity associated with the EKS Node Group. Valid values: `ON_DEMAND`, `SPOT`"
+variable "access_entries" {
+  type        = map(any)
+  default     = {}
+  description = "Map of access entries for the EKS cluster to control access on the cluster"
+}
+
+variable "public_ingress_subnets" {
   type        = string
-  default     = "ON_DEMAND"
+  description = "Public subnets to be used by ingress"
+}
+
+variable "certificate_arn" {
+  type        = string
+  description = "ACM certificate for use by ingress"
+}
+
+variable "zone_id" {
+  type        = string
+  description = "Route53 zone to create DNS names that point to ingress"
+}
+
+
+# Optional inputs. We have setup sensible defaults
+
+variable "company_name" {
+  type        = string
+  description = "To make the ELB access log bucket name unique"
+  default     = "contoso"
+}
+
+variable "sns_topic" {
+  type        = string
+  description = "SNS topic ARN for cloudwatch alarm notifications"
+  default     = null
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Used to tag resources"
+  default     = {}
+}
+
+variable "cluster_tags" {
+  type        = map(string)
+  description = "Used to tag the EKS cluster"
+  default     = {}
+}
+
+variable "metrics_type" {
+  type        = string
+  description = "cloudwatch or prometheus-grafana"
+  default     = "prometheus-grafana"
+}
+
+variable "logs_type" {
+  type        = string
+  description = "cloudwatch or elk"
+  default     = "elk"
+}
+
+variable "autoscaling_type" {
+  type        = string
+  description = "cluster-autoscaler or karpenter"
+  default     = "karpenter"
+}
+
+variable "cluster_endpoint_public_access" {
+  type        = bool
+  description = "Should the API server be in a public subnet?"
+  default     = true
 }
 
 variable "initial_nodegroup" {
-  type = any
+  type        = any
+  description = "Initial nodegroup settings"
   default = {
     "min_size"       = 1
     "max_size"       = 2
@@ -94,7 +120,8 @@ variable "initial_nodegroup" {
 }
 
 variable "critical_nodegroup" {
-  type = any
+  type        = any
+  description = "Critical nodegroup settings"
   default = {
     "min_size"       = 2
     "max_size"       = 2
@@ -105,102 +132,39 @@ variable "critical_nodegroup" {
 }
 
 variable "autoscaler_service_name" {
-  type    = string
-  default = "cluster-autoscaler-sa"
+  type        = string
+  description = "value"
+  default     = "cluster-autoscaler-sa"
 }
 
 variable "container_insights_service_name" {
-  type    = string
-  default = "container-insights"
+  type        = string
+  description = "Service account used by container insights helm chart"
+  default     = "container-insights"
 }
 
 variable "lb_service_name" {
-  type    = string
-  default = "lb-controller"
-}
-
-variable "access_entries" {
-  type        = map(any)
-  default     = {}
-  description = "Map of access entries for the EKS cluster"
+  type        = string
+  description = "Service account used by the AWS load balancer controller helm chart"
+  default     = "lb-controller"
 }
 
 variable "create_access_logs_bucket" {
+  type        = bool
   default     = true
   description = "Whether to create ELB access logs bucket or not"
 }
 
-# Argocd
-variable "public_ingress_subnets" {
-  type = string
-}
-
-variable "certificate_arn" {
-  type = string
-}
-
-variable "zone_id" {
-  type = string
-}
-
-variable "argocd_ssh_private_key" {
-  description = "The SSH private key"
-  type        = string
-}
-
-variable "argocd_slack_token" {
-  type = string
-}
-
-variable "argocd" {
-  type = any
-}
-
-variable "karpenter" {
-  type = any
-}
-
-variable "argocd_image_updater_values" {
-}
-
-# Elastic
-
-variable "elastic" {
-  type = any
-}
-
-variable "elastic_password" {
-  type = string
-}
-
-variable "kibana" {
-  type = any
-}
-
-variable "prometheus" {
-  type = any
-}
-
-variable "grafana" {
-  type = any
-}
-
 variable "create_pv_full_alert" {
-  default = false
-}
-
-variable "slack_incoming_webhook_url" {
-  type        = string
-  description = "Used by Grafana for sending out alerts."
-}
-
-variable "grafana_password" {
-  type = string
+  type        = bool
+  description = "Create an alarm to alert when a Persistent Volume is full. Not needed when using EFS which has a very high storage limit."
+  default     = false
 }
 
 variable "elb_security_policy" {
-  type    = string
-  default = "ELBSecurityPolicy-TLS13-1-2-Ext2-2021-06"
+  type        = string
+  description = "For TLS certificate"
+  default     = "ELBSecurityPolicy-TLS13-1-2-Ext2-2021-06"
 }
 
 variable "elb_access_log_expiration" {
@@ -209,10 +173,10 @@ variable "elb_access_log_expiration" {
   default     = 180
 }
 
-# Scalyr
 variable "set_up_scalyr" {
-  type    = bool
-  default = false
+  type        = bool
+  description = "Whether to use scalyr for logging"
+  default     = false
 }
 
 variable "scalyr_api_key" {
@@ -221,3 +185,74 @@ variable "scalyr_api_key" {
   default     = null
 }
 
+variable "argocd_ssh_private_key" {
+  type        = string
+  description = "The SSH private key used by ArgoCD to authenticate to Github"
+  default     = null
+}
+
+variable "argocd_slack_token" {
+  type        = string
+  description = "Used to send ArgoCD notifications to Slack"
+  default     = null
+}
+
+variable "argocd" {
+  type        = any
+  description = "Various ArgoCD settings"
+  default     = null
+}
+
+variable "karpenter" {
+  type        = any
+  description = "Various karpenter settings"
+  default     = null
+}
+
+variable "argocd_image_updater_values" {
+  type        = any
+  description = "Various ArgoCD image updater settings"
+  default     = null
+}
+
+variable "elastic" {
+  type        = any
+  description = "Various ELK settings"
+  default     = null
+}
+
+variable "elastic_password" {
+  type        = string
+  description = "Elasticache password"
+  default     = null
+}
+
+variable "kibana" {
+  type        = any
+  description = "Various Kibana settings"
+  default     = null
+}
+
+variable "prometheus" {
+  type        = any
+  description = "Various prometheus settings"
+  default     = null
+}
+
+variable "grafana" {
+  type        = any
+  description = "Various grafana settings"
+  default     = null
+}
+
+variable "slack_incoming_webhook_url" {
+  type        = string
+  description = "Used by Grafana for sending out alerts."
+  default     = null
+}
+
+variable "grafana_password" {
+  type        = string
+  description = "Grafana admin password"
+  default     = null
+}
